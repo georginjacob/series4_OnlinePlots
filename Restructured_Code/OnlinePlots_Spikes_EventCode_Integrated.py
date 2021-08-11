38dc76515afea6b233998a370688caef85389529
georginjacob <georgin@iisc.ac.in>
2021-08-11 18:05:10 +0530

    Working version: Layout changed, code cleaned

--- Restructured_Code/OnlinePlots_Spikes_EventCode_Integrated.py
+++ Restructured_Code/OnlinePlots_Spikes_EventCode_Integrated.py
@@ -5,16 +5,17 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.gridspec as gridspec
+from matplotlib.patches import Rectangle
 
 import conditionevents
 import spikeevents
 import eventcode
-
+import customlib as lib
 # Network Address
 eCubeAddress = '192.168.137.1' # IP Address for eCube or ServerNode, for directly connecting and reading DigitalPanel via the eCube API
 openephysAddress = '10.120.10.55' # OpenEphys ~0.4.2-0.4.3 EventBroadcaster module address after spike sorting
 
-global ltime, utime, nbin
+
 RelativeSpikes_all_correct   =  []
 RelativeSpikes_same_correct  =  []
 RelativeSpikes_diff_correct  =  []
@@ -37,23 +38,38 @@ test_on_ec              = eventcode.Dict['pic.testOn']
 ec_trialtype_shift      = 9000  # 9000+  1 for same , 9000 +2 for different  
 ec_trial_error_shift    = eventcode.Dict['trl.outcomeShift']   # 8500 + 0 correct, 8500 + 6 wrong 
 errorcode_correct       = 0
-errorcode_wrong         = list(range(1,9))
+errorcode_wrong         = list(range(1,9+1))
 
 
 
 # Plotting
-ltime   =   -0.5
-utime   =   3 
+sample_width=0.4 # 400 ms
+delay_between_sample_test= 0.2
+relative_time_range = (-1,2) # min time, max time relative to Sample ON. 
 nbin    =   20
-hist_bins =np.linspace(ltime,utime,nbin+1)
+hist_bins =np.linspace(relative_time_range[0],relative_time_range[1],nbin+1)
+
+# trial_counters
+tc_same_correct =1
+tc_diff_correct =1
+tc_all_correct =1
+tc_same_wrong =1
+tc_diff_wrong =1
+
 
 colors_value = ['C{}'.format(i) for i in range(6)] # Colours used
 
-ax= lib.figure_layout_creation(0) # Initializing the Layout
+(fig,ax) = lib.figure_layout_creation(0) # Initializing the Layout
+for i in range(5):
+    ax[i].add_patch( Rectangle((0.0,0.0 ),sample_width, 100,color ='k',alpha=0.25) ) # Sample ON
+    ax[i].add_patch( Rectangle((sample_width+delay_between_sample_test, 0.0),relative_time_range[1]-(sample_width+delay_between_sample_test), 100,color ='k',alpha=0.25) ) # Test ON
 ###############################################
-def animation_frame(ax):
-    global RelativeSpikes_all_correct, RelativeSpikes_same_correct, RelativeSpikes_diff_correct
 
+def animation_frame(FrameNumber):
+    global RelativeSpikes_all_correct, RelativeSpikes_same_correct, RelativeSpikes_diff_correct, ax 
+    global RelativeSpikes_same_wrong, RelativeSpikes_diff_wrong
+    global tc_same_correct,tc_diff_correct,tc_all_correct,tc_same_wrong,tc_diff_wrong
+    global relative_time_range
     # Calculate N to avoid considering synchronous updation. 
     # This current updation happens only till the first N value. Remaining will be considered during the next cycle. 
     N=len(EventCodeArray)
@@ -73,51 +89,96 @@ def animation_frame(ax):
                 EventCodeArray.pop(i)
 
             time_stop = ctrialEventCodes[-1][0]
-            trial_outcome = [(v[1]-ec_trial_error_shift) for v in (ctrialEventCodes) if (v[1]>=ec_trial_error_shift and v[1]<=ec_trial_error_shift+max(errorcode_wrong) )]
-            
-            # Correct trial
-            if(trial_outcome==0):
+            trial_outcome = [int(v[1]-ec_trial_error_shift) for v in (ctrialEventCodes) if (v[1]>=ec_trial_error_shift and v[1]<=ec_trial_error_shift+max(errorcode_wrong) )]
+
+            # Correctly responded trials (pressed either of the choices correctly)
+            if(trial_outcome[0]==0 or trial_outcome[0]==6  ):
+                
                 # sample ON, OFF, test ON
                 sampleON_time   = [v[0] for v in ctrialEventCodes   if v[1]==sample_on_ec]
-                sampleOFF_time  = [v[0] for v in ctrialEventCodes   if v[1]==sample_off_ec]
-                testON_time     = [v[0] for v in ctrialEventCodes   if v[1]==test_on_ec]
+                sampleON_time   = sampleON_time[0]
                 
+                sampleOFF_time  = [v[0] for v in ctrialEventCodes   if v[1]==sample_off_ec]
+                sampleOFF_time  = sampleOFF_time[0]
+
+                testON_time     = [v[0] for v in ctrialEventCodes   if v[1]==test_on_ec]
+                testON_time     = testON_time[0]
+
                 # extract trial type
-                trial_type = [(v[1]-ec_trialtype_shift) for v in ctrialEventCodes   if (v[1]-ec_trialtype_shift)>=0 and (v[1]-ec_trialtype_shift)<=2]
+                trial_type = [int(v[1]-ec_trialtype_shift) for v in ctrialEventCodes   if (v[1]-ec_trialtype_shift)>=0 and (v[1]-ec_trialtype_shift)<=2]
                 
                 # Spike times
-                ctrial_relative_spike_times = [v[0]-sampleON_time for i,v in enumerate(SpikeArray[:Nspike]) if (v[0]>=time_start and v[0]<=time_stop)]
+                ctrial_relative_spike_times = [v[0]-sampleON_time for i,v in enumerate(SpikeArray[:Nspike]) if (v[0]>=(sampleON_time+relative_time_range[0]) and v[0]<=min(time_stop,(sampleON_time+relative_time_range[1])))]
                 
-                # All spikes
-                RelativeSpikes_all_correct=RelativeSpikes_all_correct+ctrial_relative_spike_times # For histogram
-                ax[5].eventplot (positions = ctrial_relative_spike_times,lineoffset =len(RelativeSpikes_all_correct) ,orientation ='horizontal', linewidths =2,linelengths =1,colors=colors_value[2] )
-   
-                # SAME trial
-                if(trial_type==1):
-                    # Histogram
-                    RelativeSpikes_same_correct=RelativeSpikes_same_correct+ctrial_relative_spike_times 
-                    ax[0].clear()        
-                    ax[0].set_ylabel('Normalized Firing Rate')
-                    ax[0].set_title('SAME Trial')
-                    ax[0].hist(RelativeSpikes_same_correct,bins=hist_bins,density=True,color=colors_value[0])
+                if(trial_outcome[0]==0):
+                    # All spikes
+                    RelativeSpikes_all_correct=RelativeSpikes_all_correct+ctrial_relative_spike_times # For histogram
+                    ax[4].eventplot (positions = ctrial_relative_spike_times,lineoffset =tc_all_correct ,orientation ='horizontal', linewidths =2,linelengths =1,colors=colors_value[2] )
+                    tc_all_correct+=1
 
-                    # Raster
-                    ax[3].eventplot (positions = ctrial_relative_spike_times,lineoffset =len(RelativeSpikes_same_correct) ,orientation ='horizontal', linewidths =2,linelengths =1, colors=colors_value[0] )
+                    # SAME trial
+                    if(trial_type[0]==1):
+                        print('SAME')
+                        # Histogram
+                        RelativeSpikes_same_correct=RelativeSpikes_same_correct+ctrial_relative_spike_times 
+                        ax[0].clear()
+                        ax[0].set_title('Correct SAME Trials')
+                        ax[0].set_ylabel('Norm. Firing Rate')
+                        ax[0].set_ylim(0,1)
+                        ax[0].add_patch( Rectangle((0.0,0.0 ),sample_width, 100,color ='k',alpha=0.25) ) # Sample ON
+                        ax[0].add_patch( Rectangle((sample_width+delay_between_sample_test, 0.0),relative_time_range[1]-(sample_width+delay_between_sample_test), 100,color ='k',alpha=0.25) ) # Test ON
+                        ax[0].set_xlim(relative_time_range)
+ 
+                        
+                        
+                        ax[0].hist(RelativeSpikes_same_correct,bins=hist_bins,density=True,color=colors_value[0])
+
+                        # Raster
+                        ax[2].eventplot (positions = ctrial_relative_spike_times,lineoffset =tc_same_correct ,orientation ='horizontal', linewidths =2,linelengths =1, colors=colors_value[0] )
+                        tc_same_correct+=1
+
+                    if(trial_type[0]==2):
+                        print('DIFF')
+                        # Histogram
+                        RelativeSpikes_diff_correct=RelativeSpikes_diff_correct+ctrial_relative_spike_times 
+                        ax[1].clear()        
+                        ax[1].set_title('Correct DIFF Trials')
+                        ax[1].set_ylim(0,1)
+                        ax[1].set_yticks([])
+                        ax[1].hist(RelativeSpikes_diff_correct,bins=hist_bins,density=True,color=colors_value[1])
+                        ax[1].add_patch( Rectangle((0.0,0.0 ),sample_width, 100,color ='k',alpha=0.25) ) # Sample ON
+                        ax[1].add_patch( Rectangle((sample_width+delay_between_sample_test, 0.0),relative_time_range[1]-(sample_width+delay_between_sample_test), 100,color ='k',alpha=0.25) ) # Test ON
+                        ax[1].set_xlim(relative_time_range)
+ 
+
+                         # Raster
+                        ax[3].eventplot (positions = ctrial_relative_spike_times,lineoffset =tc_diff_correct ,orientation ='horizontal', linewidths =2,linelengths =1, colors=colors_value[1] )
+                        tc_diff_correct+=1
+
+                                   #********************************************************************************************************************************   
+                else:# Wrong Trial
+                    # SAME trial
+                    if(trial_type[0]==1):
+                        print('SAME')
+                        # Histogram
+                        RelativeSpikes_same_wrong=RelativeSpikes_same_wrong+ctrial_relative_spike_times 
 
 
-                if(trial_type==2):
-                    # Histogram
-                    RelativeSpikes_diff_correct=RelativeSpikes_diff_correct+ctrial_relative_spike_times 
-                    ax[1].clear()        
-                    ax[1].set_title('DIFF Trial')
-                    ax[1].hist(RelativeSpikes_diff_correct,bins=hist_bins,density=True,color=colors_value[1])
+                        # Raster
+                        ax[5].eventplot (positions = ctrial_relative_spike_times,lineoffset =tc_same_wrong ,orientation ='horizontal', linewidths =2,linelengths =1, colors=colors_value[0] )
+                        tc_same_wrong+=1
 
-                     # Raster
-                    ax[4].eventplot (positions = ctrial_relative_spike_times,lineoffset =len(RelativeSpikes_diff_correct) ,orientation ='horizontal', linewidths =2,linelengths =1, colors=colors_value[1] )
-            # Wrong Trial
+                    if(trial_type[0]==2):
+                        print('DIFF')
+                        # Histogram
+                        RelativeSpikes_diff_wrong=RelativeSpikes_diff_wrong+ctrial_relative_spike_times 
 
 
-###############################################
+                         # Raster
+                        ax[6].eventplot (positions = ctrial_relative_spike_times,lineoffset =tc_diff_wrong ,orientation ='horizontal', linewidths =2,linelengths =1, colors=colors_value[1] )
+                        tc_diff_wrong+=1
+
+
 pdi = conditionevents.eCubePDStream(address=eCubeAddress)
 pdi.start()
 
@@ -131,5 +192,5 @@ spikethread.start()
 time.sleep(1)
 
 # Plotting frame by frame
-animation = FuncAnimation(fig, func = animation_frame, fargs = (trial_count,ax),interval =100)
+animation = FuncAnimation(fig, func = animation_frame, interval =100)
 plt.show()
--- Restructured_Code/customlib.py
+++ Restructured_Code/customlib.py
@@ -5,13 +5,45 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
 def figure_layout_creation(fig_num=1):
-    fig = plt.figure(num=fig_num,constrained_layout=True )
+    fig = plt.figure(num=fig_num,constrained_layout=True,figsize=(10,8) )
     gs = GridSpec(4, 3, figure=fig)
     GridDef = [gs[0,0],gs[0,1],gs[1:3, 0],gs[1:3, 1],gs[:, -1],gs[-1, 0],gs[-1, -2]]  # gs[0,0] = subplot (0,0) in 4 x 3 subplot matrix.
+    
+    # 0-correct-same-hist, 1-correct-diff-hist,2- correct-same-raster, 3- correct-diff-raster
+    # 4-correct-all-raster,5-wrong-same-raster,6-wrong-diff-raster   
     ax=[]
     for i in range(7):
         ax.append(fig.add_subplot(GridDef[i]))
-  
+        ax[i].set_xlim(-1,2)
+        if(i==2 or i==3): # same and diff correct raster
+            ax[i].set_ylim(1,40)
+            ax[i].set_yticks([])
+        if(i==4): # All raster
+            ax[i].set_ylim(1,80)
+            ax[i].set_yticks([])
+        if(i==5 or i==6): # wrong raster
+            ax[i].set_ylim(1,20)
+            ax[i].set_yticks([])
+        if (i==2 or i==4 or i==5):
+            ax[i].set_ylabel('Trial count ->')
+        if(i==4 or i==5 or i==6):
+            ax[i].set_xlabel('Relative Times (s)')
+
+    ax[0].set_title('Correct SAME Trials')
+    ax[0].set_ylabel('Norm. Firing Rate')
+    ax[0].set_ylim(0,1)
+
+    ax[1].set_title('Correct DIFF Trials')
+    ax[1].set_ylim(0,1)
+    ax[1].set_yticks([])
+
+    ax[4].set_title('ALL Trials')
+
+    ax[5].set_title('Wrong SAME Trials')
+    ax[6].set_title('Wrong DIFF Trials')
+
     plt.show(block=False) # Block = False avoids the halt at plot
-    return ax
+    return fig, ax
+
+
 
